59d4bc7273b6a80dc2a8ef009a5a81238f7d496a
lemontine <computereality@gmail.com>
2024-11-02 14:58:31 -0700

    prototype movement + player

---
+++ scripts/camera.gd
@@ -0,0 +1,15 @@
+extends Camera3D
+
+var player: Node = null
+var offset: Vector3 = Vector3()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = $"../Player" # Adjust the path to your player node
+	if player:
+		offset = global_transform.origin - player.global_transform.origin
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player:
+		global_transform.origin = player.global_transform.origin + offset
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,51 @@
+class_name Player
 extends CharacterBody3D
 
+@export var speed = 1.7
+@onready var anim = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
 
-@export var SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-@onready var animPlayer = $AnimationPlayer
-@onready var animTree = $AnimationTree
-@onready var animState = animTree.get("parameters/playback")
+var can_interact: bool = false
+var last_direction = Vector2.ZERO
 
+func handle_movement():
+	var input_vector = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if Input.is_action_pressed("ui_up"):
+		input_vector.y -= 1
+	elif Input.is_action_pressed("ui_down"):
+		input_vector.y += 1
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("ui_left"):
+		input_vector.x -= 1
+	elif Input.is_action_pressed("ui_right"):
+		input_vector.x += 1
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	input_vector = input_vector.normalized()
+	
+	# Apply some inertia for a slippery effect
+	var target_velocity = Vector3(input_vector.x * speed, 0, input_vector.y * speed)
+	velocity = velocity.lerp(target_velocity, 0.1)  # Adjust the factor for more or less slipperiness
+	
+	print(input_vector)
+
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
+		last_direction = input_vector
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		animationTree.set("parameters/Idle/blend_position", last_direction)
+		animationState.travel("Idle")
 
+func _physics_process(delta):
+	handle_movement()
 	move_and_slide()
+
+#
+#func _on_InteractBox_body_entered(body):
+	#if body.is_in_group("Dialog"):
+		#can_interact = true
+#func _on_InteractBox_body_exited(body):
+	#if body.is_in_group("Dialog"):
+		#can_interact = false
