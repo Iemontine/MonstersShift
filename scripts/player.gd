3e8590c66c1acc859d78fe8282b7bb9d6b1d9bf0
lemontine <computereality@gmail.com>
2024-11-05 23:40:05 -0800

    top down proof of concept with object and background tileset

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 class_name Player
-extends CharacterBody3D
+extends CharacterBody2D
 
-@export var speed = 1.7
+@export var speed = 100
 @export var camera: NodePath
 
 
@@ -27,33 +27,14 @@ func handle_movement():
 		input_vector.x += 1
 
 	input_vector = input_vector.normalized()
-	
-	# Apply some inertia for a slippery effect
-	var target_velocity = Vector3(input_vector.x * speed, 0, input_vector.y * speed)
-	
-	# Adjust movement direction relative to camera
-	var camera_node = get_node(camera)
-	if camera_node:
-		var camera_forward = camera_node.global_transform.basis.z  # Invert the forward vector
-		var camera_right = camera_node.global_transform.basis.x
-		var direction = (camera_right * target_velocity.x + camera_forward * target_velocity.z).normalized()
-		
-		velocity = velocity.lerp(Vector3(direction.x, 0, direction.z) * speed, 0.1)	# slippery velocity
-		
-		print(input_vector)
 
-		if input_vector != Vector2.ZERO:
-			animationTree.set("parameters/Walk/blend_position", input_vector)
-			animationState.travel("Walk")
-			last_direction = input_vector
-		else:
-			animationTree.set("parameters/Idle/blend_position", last_direction)
-			animationState.travel("Idle")
-		
-		# Rotate player to face the camera
-		look_at(camera_node.global_transform.origin, Vector3.UP)
+	if Input.is_key_pressed(KEY_SHIFT):
+		velocity = input_vector * speed * 2
 	else:
-		print("Camera node not found")
+		velocity = input_vector * speed
+
+
+	animationTree.set("parameters/walk/blend_position", input_vector)
 
 
 func _physics_process(delta):
