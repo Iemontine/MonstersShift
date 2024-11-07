6988ca9d5903c44248af747851a71082345c4615
lemontine <computereality@gmail.com>
2024-11-06 17:18:28 -0800

    imported assets, starter neighborhood, developing interaction framework

---
+++ scripts/camera.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+@export var target: Player
+
+func _physics_process(_delta: float) -> void:
+	if target:
+		position = target.position
+
+		if Input.is_key_pressed(KEY_KP_ADD) or Input.is_key_pressed(KEY_EQUAL):
+			zoom += Vector2(0.1, 0.1)
+		elif Input.is_key_pressed(KEY_KP_SUBTRACT) or Input.is_key_pressed(KEY_MINUS):
+			zoom -= Vector2(0.1, 0.1)
+
+		zoom.x = clamp(zoom.x, 0.5, 7)
+		zoom.y = clamp(zoom.y, 0.5, 7)	# 2 looks good as min
--- scripts/player.gd
+++ scripts/player.gd
@@ -29,14 +29,31 @@ func handle_movement():
 	input_vector = input_vector.normalized()
 
 	if Input.is_key_pressed(KEY_SHIFT):
-		velocity = input_vector * speed * 2
+		# velocity = input_vector * speed * 2
+		velocity = input_vector * speed * 10
 	else:
 		velocity = input_vector * speed
+
 
 
 	animationTree.set("parameters/walk/blend_position", input_vector)
 
 
-func _physics_process(delta):
+func handle_interaction():
+	var direction = last_direction.normalized()
+	var position_in_front = global_position + direction * 16  # Assuming tile size is 16x16
+	var tilemap_layer = get_parent().get_node("objects")  # Ensure this is a TileMapLayer node
+	if tilemap_layer is TileMapLayer:
+		var cell = tilemap_layer.local_to_map(position_in_front)
+		print(cell)
+
+		#print("Interacted with tile ID: ", tile_id)
+	else:
+		print("The node 'objects' is not a TileMapLayer")
+		
+
+func _physics_process(_delta):
 	handle_movement()
 	move_and_slide()
+	if Input.is_action_just_pressed("ui_accept"):
+		handle_interaction()
