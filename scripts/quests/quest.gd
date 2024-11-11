0a40874beb5cdeaf610ea4be9fa6d40c44e4fdb1
Matt F <mpfulde@ucdavis.edu>
2024-11-11 13:41:07 -0800

    added quest finishing logic

--- scripts/quests/quest.gd
+++ scripts/quests/quest.gd
@@ -1,6 +1,18 @@
 extends Node
 class_name Quest
 
+var finished : bool
+
+func _process(_delta: float) -> void:
+	pass
+
 # override when making an actual quest
 func get_quest_name() -> String:
 	return "null quest"
+
+# unsure of what type this should be
+func get_rewards() -> int:
+	return 0;
+
+func done() -> bool:
+	return finished
--- scripts/quests/quest_manager.gd
+++ scripts/quests/quest_manager.gd
@@ -29,3 +29,16 @@ func set_active_quest(quest:Quest) -> void:
 		return
 	
 	_active_quest = quest.get_quest_name()
+
+func finish_quest(quest:Quest):
+	if quest.get_quest_name() == "null quest":
+		printerr("cannot fininish a null quest")
+		return
+	elif not quest_list.has(quest.get_quest_name()):
+		printerr("cannot finish a quest that is not in the list")
+		return
+	
+	if quest.done():
+		# give player rewards
+		var rewards := quest.get_rewards()
+		quest.queue_free()
