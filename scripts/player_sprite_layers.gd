4896813d971013eca852dc74e7c2d4a3f5971687
Darroll <computereality@gmail.com>
2024-11-20 16:29:38 -0800

    character customization dev, remove MSCA i hope that didn't break anything

--- scripts/player_sprite_layers.gd
+++ scripts/player_sprite_layers.gd
@@ -6,7 +6,142 @@ signal animation_state_started(state_name)
 signal animation_set_hitbox(counter, track, timer_value, direction)
 signal make_sound(state_name, sound_name)
 
-var shader = preload("res://addons/msca/shader/simple_ramp_shader.gdshader")
+var shader = preload("res://shaders/simple_ramp_shader.gdshader")
+
+@export var color_00undr: Color
+@export var color_01body: Color
+@export var color_02sock: Color
+@export var color_03fot1: Color
+@export var color_04lwr1: Color
+@export var color_05shrt: Color
+@export var color_06lwr2: Color
+@export var color_07fot2: Color
+@export var color_08lwr3: Color
+@export var color_09hand: Color
+@export var color_10outr: Color
+@export var color_11neck: Color
+@export var color_12face: Color
+@export var color_13hair: Color
+@export var color_14head: Color
+@export var color_15over: Color
+
+const DEFAULT_COLORS = [
+	Color("#58E0A0"),
+	Color("#289860"),
+	Color("#205040"),
+	Color("#181818"),
+	Color("#F8A070"),
+	Color("#D84848"),
+	Color("#942132"),
+	Color("#181818")
+]
+
+var palettes
+
+func load_palettes():
+	var farmer_base_path = "res://assets/characters/farmer_base/"
+	palettes = {
+		"3color": {
+			"base_file": farmer_base_path + "_supporting files/palettes/base ramps/3-color base ramp (00a).png",
+			"file": farmer_base_path + "_supporting files/palettes/mana seed 3-color ramps.png",
+			"base": null,
+			"palettes": null
+		},
+		"4color": {
+			"base_file": farmer_base_path + "_supporting files/palettes/base ramps/4-color base ramp (00b).png",
+			"file": farmer_base_path + "_supporting files/palettes/mana seed 4-color ramps.png",
+			"base": null,
+			"palettes": null
+		},
+		"hair": {
+			"base_file": farmer_base_path + "_supporting files/palettes/base ramps/hair color base ramp.png",
+			"file": "",
+			"base": null,
+			"palettes": null
+		},
+		"skin": {
+			"base_file": farmer_base_path + "_supporting files/palettes/base ramps/skin color base ramp.png",
+			"file": "",
+			"base": null,
+			"palettes": null
+		},
+		"weapon_tool": {
+			"base_file": farmer_base_path + "_supporting files/palettes/base ramps/weapon and tool color base ramps.png",
+			"file": "",
+			"base": null,
+			"palettes": null
+		}
+	}
+	for bp in palettes.values():
+		var base_texture = load(bp["base_file"])
+		bp["base"] = MSCAPaletteSwaps.create_palette_from_image(base_texture.get_image())
+		if bp["file"] != "":
+			bp["palettes"] = []
+			var _palettes = load(bp["file"])
+			var palettes_image = _palettes.get_image()
+			var palettes_count = palettes_image.get_height() / 2
+			for p in palettes_count:
+				var start_y = p * 2
+				var palette_image = palettes_image.get_region(Rect2i(0, start_y, palettes_image.get_width(), 2))
+				var new_palette = MSCAPaletteSwaps.create_palette_from_image(palette_image)
+				if new_palette.size() > 0:
+					bp["palettes"].append(new_palette)
+
+func _ready():
+	load_palettes()
+
+func _physics_process(_delta: float) -> void:
+	set_layer_shader("00undr", color_00undr)
+	set_layer_shader("01body", color_01body)
+	set_layer_shader("02sock", color_02sock)
+	set_layer_shader("03fot1", color_03fot1)
+	set_layer_shader("04lwr1", color_04lwr1)
+	set_layer_shader("05shrt", color_05shrt)
+	set_layer_shader("06lwr2", color_06lwr2)
+	set_layer_shader("07fot2", color_07fot2)
+	set_layer_shader("08lwr3", color_08lwr3)
+	set_layer_shader("09hand", color_09hand)
+	set_layer_shader("10outr", color_10outr)
+	set_layer_shader("11neck", color_11neck)
+	set_layer_shader("12face", color_12face)
+	set_layer_shader("13hair", color_13hair)
+	set_layer_shader("14head", color_14head)
+	set_layer_shader("15over", color_15over)
+
+func set_layer_shader(layer_name: String, base_color: Color):
+	var layer_node = get_node_or_null(layer_name)
+	if layer_node == null: return
+	var shader_material = ShaderMaterial.new()
+	shader_material.shader = shader
+	var default_colors = get_default_colors_for_layer(layer_name)
+	var colors = generate_color_ramp(base_color, default_colors.size())
+	for i in range(default_colors.size()):
+		var original_color = default_colors[i]
+		var replace_color = colors[i] if colors.size() > i else original_color
+		shader_material.set_shader_parameter("original_" + str(i), original_color)
+		shader_material.set_shader_parameter("replace_" + str(i), replace_color)
+	layer_node.material = shader_material
+
+func generate_color_ramp(base_color: Color, num_colors: int) -> Array:
+	var colors = [base_color]
+	var step = 1.0 / (num_colors - 1)
+	for i in range(1, num_colors):
+		var darker_color = base_color.lerp(Color("#181818"), step * i)
+		colors.append(darker_color)
+	return colors
+
+func get_default_colors_for_layer(layer_name: String) -> Array:
+	match layer_name:
+		"13hair":
+			return palettes["hair"]["base"]
+		"12face", "09hand", "01body":
+			return palettes["skin"]["base"]
+		"10outr", "11neck", "14head", "15over":
+			return palettes["4color"]["base"]
+		"00undr", "02sock", "03fot1", "04lwr1", "05shrt", "06lwr2", "07fot2", "08lwr3":
+			return palettes["3color"]["base"]
+		_:
+			return DEFAULT_COLORS
 
 func set_corresponding_layers_to_animframe(animframe:int, flipped:bool = false):
 	#the Animation Tree must be disabled when you use this
@@ -31,27 +166,3 @@ func set_hitbox(counter, track, timer_value, direction):
 
 func emit_sound(state_name:String, sound_name:String):
 	emit_signal("make_sound", state_name, sound_name)
-
-#get the name and the path
-func get_full_spritesheet_name_and_path(layer, type, version):
-	var path = layer + "/" + get_full_spritesheet_name(layer, type, version)
-	return path
-
-func get_full_spritesheet_name(layer, type, version):
-	#some stuff like the tools have a single page they are checked, you can force the page for the name
-	return "fbas_"+layer+"_"+type+"_"+version
-
-func set_shader(layer, original_ramp, new_ramp):
-	original_ramp = MSCAPaletteSwaps.html_to_color_palette(original_ramp)
-	new_ramp = MSCAPaletteSwaps.html_to_color_palette(new_ramp)
-	var shader_material = null
-	var layer_node = get_node_or_null(layer)
-	if layer_node == null: return
-	shader_material = ShaderMaterial.new()
-	shader_material.shader = shader
-	var p_count = 0
-	for p in new_ramp:
-		shader_material.set_shader_parameter("original_"+str(p_count),original_ramp[p_count])
-		shader_material.set_shader_parameter("replace_"+str(p_count),p)
-		p_count = p_count +1
-	layer_node.material = shader_material
