f695eefcedd74f60ad92346840cafb47eac6984f
lemontine <computereality@gmail.com>
2024-11-23 12:18:25 -0800

    optimize atlas, continued town collisions + decorating dev, fix camera, fix door collisions

--- prefabs/scripts/camera.gd
+++ prefabs/scripts/camera.gd
@@ -4,15 +4,15 @@ extends Camera2D
 @export var target: Player
 
 
-func _physics_process(_delta: float) -> void:
+func _process(_delta: float) -> void:
 	if target:
 		
 		position = target.position
 
 		if Input.is_key_pressed(KEY_KP_ADD) or Input.is_key_pressed(KEY_EQUAL):
-			zoom += Vector2(0.1, 0.1)
+			zoom += Vector2(0.03, 0.03)
 		elif Input.is_key_pressed(KEY_KP_SUBTRACT) or Input.is_key_pressed(KEY_MINUS):
-			zoom -= Vector2(0.1, 0.1)
+			zoom -= Vector2(0.03, 0.03)
 
 		zoom.x = clamp(zoom.x, 0.5, 7)
 		zoom.y = clamp(zoom.y, 0.5, 7)	# 2 looks good as min
