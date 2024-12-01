1a134602f931f0d0ef041aa67186e1bf7972d650
bfedkiw6 <bnfedkiw@ucdavis.edu>
2024-11-30 17:21:56 -0800

    Added a cutscene trigger outside of bakery - not tested yet

---
+++ cutscenes/outside_bakery_cutscene.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+const Event = preload("res://scripts/story_manager.gd").Event
+
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	
+	
+func _on_body_entered(_body:Player) -> void:
+	StoryManager.transition_to_event(Event.OUTSIDE_BAKERY)
