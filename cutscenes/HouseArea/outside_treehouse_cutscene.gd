019929de7d531e9a62eba59ba3c87c29bc378298
bfedkiw6 <bnfedkiw@ucdavis.edu>
2024-12-03 18:17:40 -0800

    Tried to fix leave early issue

--- cutscenes/HouseArea/outside_treehouse_cutscene.gd
+++ cutscenes/HouseArea/outside_treehouse_cutscene.gd
@@ -6,7 +6,11 @@ func _ready() -> void:
 	
 	
 func _on_body_entered(_body:Object) -> void:
-	if _body is Player and StoryManager.current_event == StoryManager.Event.INTRO:
+	if (
+		_body is Player 
+		and (StoryManager.current_event == StoryManager.Event.INTRO 
+		or StoryManager.current_event == StoryManager.Event.LEAVE_TOO_EARLY)
+	):
 		StoryManager.transition_to_event(StoryManager.Event.ARRIVAL_START_OUTSIDE)
 		PlayerController.start_cutscene("arrival_start_outside")
 		queue_free()
