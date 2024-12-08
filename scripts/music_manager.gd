9e873b74583c7300a18d066a62f15821805e904e
Matt F <mpfulde@ucdavis.edu>
2024-12-07 23:57:32 -0800

    added logic for all music cases and temporary names for tracks, TODO find tracks

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -1,20 +1,21 @@
 extends Node
 
 # dictionary of tracks to their associated scenes, { scene:track }
-# TODO: make these line up with actual scene names
+# TODO: make sure non outside tracks line up with scene names
+# TODO: FIND TRACKS
 var _tracks := {
-	"Outside Day" : "",
-	"Outside Evening" : "",
-	"Outside Night" : "",
-	"Bakery" : "",
-	"Bakery Game Day": "",
-	"Bakery Game Night": "",
-	"Conbini" : "",
-	"Treehouse" : ""
+	"Outside Day" : "outside_day.wav",
+	"Outside Evening" : "outside_eve.wav",
+	"Outside Night" : "outside_night.wav",
+	"bakery" : "bakery_game_day.wav",
+	"bakery_no_game": "bakery.wav",
+	"treehouse_interior_baker_game": "bakery_game_night.wav",
+	"conbini" : "conbini.wav",
+	"treehouse_interior" : "house.wav"
 }
 var _current_track : String
 var _track_path := "res://assets/sound/music/"
-var _use_custom_track : bool = true
+var _use_custom_track : bool = false
 var _custom_track : String = "test_audio.WAV"
 var _current_playtime : float = 0.0
 
@@ -44,22 +45,33 @@ func _on_scene_transition_completed() -> void:
 			pass
 		pass
 	elif not _check_ouside():
-		if _current_track != _tracks[SceneManager.current_scene]:
-			stream_player.play()
-			pass
+		var stream = load(_track_path + _tracks[SceneManager.current_scene.to_lower()])
+		stream_player.stream = stream
+		if _current_track != _tracks[SceneManager.current_scene.to_lower()]:
+			_current_playtime = 0.0
+			_current_track = _tracks[SceneManager.current_scene.to_lower()]
 
 			
 	else:
 		match SceneManager.time_of_day:
 			SceneManager.TIME.DAY:
+				var stream = load(_track_path + _tracks["Outside Day"])
+				stream_player.stream = stream
 				if _current_track != _tracks["Outside Day"]:
-					pass
+					_current_playtime = 0.0
+					_current_track = _tracks["Outside Day"]
 			SceneManager.TIME.EVENING:
+				var stream = load(_track_path + _tracks["Outside Evening"])
+				stream_player.stream = stream
 				if _current_track != _tracks["Outside Evening"]:
-					pass
+					_current_playtime = 0.0
+					_current_track = _tracks["Outside Evening"]
 			SceneManager.Time.NIGHT:
-				if _current_track != _tracks["Outside Day"]:
-					pass
+				var stream = load(_track_path + _tracks["Outside Night"])
+				stream_player.stream = stream
+				if _current_track != _tracks["Outside Night"]:
+					_current_playtime = 0.0
+					_current_track = _tracks["Outside Night"]
 		
 		
 	
@@ -76,4 +88,4 @@ func end_custom_track() -> void:
 	_custom_track = ""
 	
 func _check_ouside() -> bool:
-	return SceneManager.current_scene == "Treehouse_Exterior" or SceneManager.current_scene == "Town"
+	return SceneManager.current_scene.to_lower() == "treehouse_exterior" or SceneManager.current_scene.to_lower() == "town"
