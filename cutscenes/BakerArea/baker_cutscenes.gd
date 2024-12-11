864f912dc294f140ec4de0377656e3785787c3ae
bfedkiw6 <bnfedkiw@ucdavis.edu>
2024-12-11 11:32:10 -0800

    Added baker dialogue for second day

--- cutscenes/BakerArea/baker_cutscenes.gd
+++ cutscenes/BakerArea/baker_cutscenes.gd
@@ -4,3 +4,6 @@ func _on_interacted() -> void:
 	if StoryManager.current_event == StoryManager.Event.FIRST_ENTER_BAKERY:
 		StoryManager.transition_to_event(StoryManager.Event.BAKER_FIRST_INTERACTION)
 		PlayerController.start_cutscene("baker_first_interaction")
+	if StoryManager.current_event >= StoryManager.Event.DAY_TWO_MORNING:
+		StoryManager.transition_to_event(StoryManager.Event.BAKER_DAY_TWO)
+		PlayerController.start_cutscene("baker_day_two")
