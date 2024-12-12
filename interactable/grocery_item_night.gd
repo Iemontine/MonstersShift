cd25ff723afde8922a4d2d99521c1b9e928468e8
Matt F <mpfulde@ucdavis.edu>
2024-12-12 03:46:12 -0800

    added night trigger for conbini night item, added locking to night time conbini

--- interactable/grocery_item_night.gd
+++ interactable/grocery_item_night.gd
@@ -4,7 +4,6 @@ class_name GroceryItemNight
 
 #TODO: add triggers for visibility and enabled
 
-
 func show_grocery_ui():
 	super()
 	for child in grocery_ui.get_children():
@@ -13,9 +12,14 @@ func show_grocery_ui():
 	grocery_ui.get_node("Paper").visible = false
 	var canvas = get_tree().current_scene.get_node_or_null("CanvasModulate")
 	canvas.color = Color(0.121, 0.173, 0.367) * 0.7
-	
+
+func enable_grocery_item():
+	enabled = true
+	visible = true
+
 func _on_yes_pressed() -> void:
 	super()
 	enabled = false
 	visible = false
 	get_tree().current_scene.get_node_or_null("CanvasModulate").color = Color(0.121, 0.173, 0.367)
+	StoryManager.transition_to_event(StoryManager.Event.GRABBED_NIGHT_ITEM)
